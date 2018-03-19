d78b726c9571dabbfe6810d2e5db6b2fdcbe0186
gregsakai <gregsakai@gmail.com>
2018-03-19 11:32:53 -0700

    rooms working, TODO: fix positioning of players

--- src/comp/Landing.js
+++ src/comp/Landing.js
@@ -13,7 +13,10 @@ class Landing extends Component {
             <div id="mainPage">
               <div id="mvtInfo">MOTOR VEHICKLE FEFT IS THE SICKSTH ITTERATION OF THE POPULAR CAR STEALING GAME MOTER VHIKKEL THEPHTD. ITS GOT AKSHIN PACKED CAR STEELING AND DRIVING. PLAY WITH YOURE FEIRNDS OR ALONE. YOU WILL HAVE LOTS OF FUNS.
                 <br />
-                <button onClick={this.props.handleDisplay.bind(this, "room1")} id="mvtBut">ENTER</button>
+                <button onClick={this.props.handleDisplay.bind(this, "room1")} className="mvtBut">Room 1</button>
+                <button onClick={this.props.handleDisplay.bind(this, "room2")} className="mvtBut">Room 2</button>
+                <button onClick={this.props.handleDisplay.bind(this, "room3")} className="mvtBut">Room 3</button>
+
               </div>
               </div>
               <div id="mvtLogo">
